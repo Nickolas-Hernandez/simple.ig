816b293f9f357134c05311ed6f2fd0cd0e3b2271
Nick Hernandez <nickhernandez@Nicks-Mac-mini.local>
2023-07-18 20:23:26 -0700

    Create post componet and test import in page

---
+++ src/components/Post.svelte
@@ -0,0 +1,15 @@
+<script>
+    import profileImage from '$lib/assets/logo.png'
+
+    const postData = {
+        user: "LuckyCharms",
+        profileImage: ""
+    }
+
+    console.log(profileImage);
+
+    // export let prop1
+</script>
+
+<h1>Post component</h1>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script>
+    import Post from '../components/Post.svelte';
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="">
+    <Post />
+</div>
