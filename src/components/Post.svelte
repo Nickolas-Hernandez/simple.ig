b275d39da722934e398af33907a2f1d98092ddca
Nick Hernandez <nickhernandez@Nicks-Mac-mini.local>
2023-07-19 22:10:46 -0700

    Render an image

--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -1,15 +1,25 @@
 <script>
-    import profileImage from '$lib/assets/logo.png'
+    import profileImage from '$lib/assets/profile-image.jpg'
 
     const postData = {
         user: "LuckyCharms",
-        profileImage: ""
+        profileImage: profileImage
     }
-
-    console.log(profileImage);
-
-    // export let prop1
 </script>
 
-<h1>Post component</h1>
+<section>
+    <div class="image-frame" style="background-image: url({postData.profileImage})">
+        
+    </div>
+</section>
+ 
 
+ <style>
+    .image-frame {
+        width: 100%;
+        height: 400px;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+    }
+ </style>
