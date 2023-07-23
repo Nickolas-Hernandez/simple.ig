7296ca0399db1e9577ebefde995438929c94b6b6
Nick Hernandez <nickhernandez@Nicks-Mac-mini.local>
2023-07-22 19:29:46 -0700

    update front end

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,44 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+let username = '';
+let password = '';
+
+
+async function handleLogin(e) {
+  e.preventDefault();
+  const response = await fetch('/api/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: 'example_user',
+      password: 'example_password'
+    })
+  });
+
+  if (response.ok) {
+    // Handle successful login, e.g., store authentication token in browser's local storage
+    console.log('success!');
+  } else {
+    // Handle login failure, e.g., show an error message to the user
+    console.log('fail :(');
+  }
+}
+
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <section class="login">
+    <h1>Login</h1>
+    <form on:submit={handleLogin} id="login">
+      <input type="text" id="username" name="username" required>
+      <input type="password" id="password" name="password" required>
+      <input type="submit" value="login">
+    </form>
+  </section>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
 </style>
