0178fe71493a8763a98ad56cfa273788ea2a7be8
Nick Hernandez <nickhernandez@Nicks-Mac-mini.local>
2023-07-24 20:21:57 -0700

    Restart: Env variable set up

--- server/main.go
+++ server/main.go
@@ -1,56 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
-	"simple.ig/database"
-	_ "simple.ig/models"
-
-	"database/sql"
-	"fmt"
-	"io/ioutil"
-
-	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
 )
 
 func main() {
-	loadEnv()
-	loadDatabase()
-
-	router := gin.Default()
-	// router.GET("/api/ping", pong)
-	// router.POST("/api/user/register", registerUser(db))
-
-	router.Run("localhost:8080")
-}
-
-func pong(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "pong",
-	})
-}
-
-func registerUser(c *gin.Context, db *sql.DB) {
-	body, err := ioutil.ReadAll(c.Request.Body)
+	// set up env variables
+	err := godotenv.Load("../.env.local")
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal("Error loading .env.local file")
 	}
-	fmt.Println(string(body))
-	// connect to database
-	// add new user to database
-	// return success or failure
 
-}
+	// dbHost := os.Getenv("DB_HOST")
 
-func loadDatabase() {
-	database.Connect()
-}
+	// Create db connection
+	// Create router
+	// Create routes
+	// Start server
 
-func loadEnv() {
-	err := godotenv.Load(".env.local")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	fmt.Println("Hello World!")
 }
