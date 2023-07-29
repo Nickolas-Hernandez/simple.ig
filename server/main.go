d00bc846bbcd072fd7ae75edc04e79dc7e504d18
Nick Hernandez <nickhernandez@Nicks-Mac-mini.local>
2023-07-29 10:46:56 -0700

    Pass db instance to routes

--- server/main.go
+++ server/main.go
@@ -5,11 +5,24 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 )
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+}
+
+// var db *pgx.Conn
+
+type App struct {
+	DB *pgx.Conn
+}
 
 func main() {
 	// set up env variables
@@ -25,25 +38,52 @@ func main() {
 	}
 	defer conn.Close(context.Background())
 
+	db := App{
+		DB: conn,
+	}
+
 	// Create router
-	app := fiber.New()
-
-	// Create routes
-	app.Get("/api/ping", pingHandler)
-	app.Post("/api/auth/register", loginHandler)
-
-	// Start server
-	app.Listen(":3000")
-
-	fmt.Println("Hello World!")
+	startServer(db)
 }
 
-func loginHandler(c *fiber.Ctx) error {
-	c.Accepts("application/json")
-	body := string(c.Body())
-	fmt.Println("Request body: ", body)
-	c.Status(fiber.StatusOK)
+func startServer(db App) {
+	app := fiber.New()
+
+	app.Get("/ping", pingHandler)
+	app.Post("/api/auth/register", db.loginHandler)
+
+	app.Listen(":3000")
+}
+
+func (db *App) loginHandler(c *fiber.Ctx) error {
+	fmt.Println("hello")
+	var user User
+	err := c.BodyParser(&user)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	fmt.Println(user.Username)
+	fmt.Println(user.Password)
+	fmt.Println(user.Email)
+
+	if strings.TrimSpace(user.Username) == "" || strings.TrimSpace(user.Password) == "" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Username and password are required",
+		})
+	}
+
+	// query := "INSERT INTO users (username, password) VALUES ($1, $2)"
+	// _, err = db.Exec(context.Background(), query, username, hashedPassword)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// body := string(c.Body())
+	// fmt.Println("Request body: ", body)
+	// c.Status(fiber.StatusOK)
 	return c.SendString("Body has been logged")
+
 }
 
 func pingHandler(c *fiber.Ctx) error {
