31531257a76034d0c94d6fbde9e111c43c41c605
Nick Hernandez <nickhernandez@Nicks-Mac-mini.local>
2023-09-10 22:15:32 -0700

    update login class names and add height to main

--- src/App.svelte
+++ src/App.svelte
@@ -42,20 +42,26 @@ async function handleLogin(e) {
 </script>
 
 <main>
-  <section class="login w-full flex flex-col items-center">
-    <div class="column w-5/6 md:4/5 lg:w-1/3">
-      <form on:submit={handleLogin} id="login" class="bg-blue-100 flex flex-col px-20 text-center">
-        <h1>Svelte.IG</h1>
-        <input type="text" id="username" name="username" bind:value={username} required>
-        <input type="text" id="email" name="email" bind:value={email} required>
-        <input type="password" id="password" name="password" bind:value={password} required>
-        <input type="submit" value="Ping">
+  <section class="login login__container w-full flex flex-col items-center">
+    <div class="login__column column w-5/6 md:4/5 lg:w-1/3">
+      <form on:submit={handleLogin} id="login" class="login-form bg-blue-100 flex flex-col px-20 py-2 text-center border border-black">
+        <h1 class="login-form__logo">Svelte.IG</h1>
+        <input class="login-form__username-input" type="text" id="username" name="username" bind:value={username} required>
+        <input class="login-form__email-input" type="text" id="email" name="email" bind:value={email} required>
+        <input class="login-form__password-input" type="password" id="password" name="password" bind:value={password} required>
+        <input class="login-form__" type="submit" value="Ping">
       </form>
     </div>
   </section>
 </main>
 
 <style>
+
+  main {
+    height: 100%;
+    min-height: 100vh;
+    max-height: 100vh;
+  }
 
   .login form input {
     margin-bottom: 6px;
